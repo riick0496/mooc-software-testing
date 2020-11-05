8384173f6a87e59acdcbbe84c6ec0f20853c4479
riick0496 <ricardobonillahdz@hotmail.com>
2020-11-04 22:04:45 -0500

    Add GHappy Test

--- src/main/java/tudelft/ghappy/GHappy.java
+++ src/main/java/tudelft/ghappy/GHappy.java
@@ -4,15 +4,21 @@ public class GHappy {
 
     public boolean gHappy(String str) {
         assert str!=null;
-        for(int i = 0; i < str.length(); i++) {
-            if(str.charAt(i) == 'g') {
-                if (i >= 0 && str.charAt(i-1) == 'g') { continue; }
-                if (i+1 < str.length() && str.charAt(i+1) == 'g') { continue; }
-                return false;
+        if(str.length() > 1) {
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == 'g') {
+                    if (i > 0 && str.charAt(i - 1) == 'g') {
+                        continue;
+                    }
+                    if (i + 1 < str.length() && str.charAt(i + 1) == 'g') {
+                        continue;
+                    }
+                    return false;
+                }
             }
+            return true;
+        }else{
+            return false;
         }
-
-        return true;
-
     }
 }
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,67 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+    GHappy happy;
+    boolean result;
+    public GHappyTest()
+    {
+        happy = new GHappy();
+    }
+    @Test
+    public void WithoutGHappyOneG() {
+        result = happy.gHappy("g");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void OneGHappy()
+    {
+        result = happy.gHappy("gg");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void WithoutGHappy()
+    {
+        result = happy.gHappy("x");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void TwoGHappy()
+    {
+        result = happy.gHappy("xggxxggxx");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void TwoGHappyExtended()
+    {
+        result = happy.gHappy("xggxxgyxxgg");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void MultipleGHappy()
+    {
+        result = happy.gHappy("gggggggg");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void VoidGHappy()
+    {
+        result = happy.gHappy("");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void MultipleLettersGHappy()
+    {
+        result = happy.gHappy("xyyggddfsgggxyhtrxikgggulouggoñpñixññiñoitggghbrggx");
+        Assertions.assertTrue(result);
+    }
 }
